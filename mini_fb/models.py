5a444b59831e19cc1b78a8dbc13fbef83f350546
jaimeyfrank <franjaim21@gmail.com>
2024-10-07 18:45:09 -0400

    finished mini fb

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -7,7 +7,7 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     email = models.EmailField()
-    profile_picture = models.ImageField(upload_to='profile_pictures')
+    profile_picture = models.URLField(blank=True)
     
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('', views.main, name='main'),
+    path('', views.ShowAllProfilesView.as_view(), name='show_all'),
 ]

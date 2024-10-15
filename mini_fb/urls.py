7b0c726b87a3c820f9ed993162d045e73d63765f
jaimeyfrank <franjaim21@gmail.com>
2024-10-15 16:57:03 -0400

    a6 tasks 3 and 4

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.conf import settings
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.ShowAllProfilesView.as_view(), name='show_all'),
-    path('profile/<int:pk>', views.ShowProfilePageView.as_view(), name='show_profile'),
+    path('', ShowAllProfilesView.as_view(), name='show_all'),
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name='show_profile'),
+    path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
+    path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from typing import Any
+
+from django.views.generic import ListView, DetailView, CreateView
 from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -15,3 +19,34 @@ class ShowProfilePageView(DetailView):
     model = Profile
     template_name = 'mini_fb/show_profile.html'
     context_object_name = 'profile'
+
+class CreateProfileView(CreateView):
+    '''the view to create a profile'''
+    form_class = CreateProfileForm
+    template_name = 'mini_fb/create_profile_form.html'
+    
+    def get_success_url(self) -> str:
+        '''Return the URL to redirect to on success.'''
+       # Use the newly created profile's pk
+        return reverse('show_profile', kwargs={'pk': self.object.pk})
+    
+class CreateStatusMessageView(CreateView):
+    '''the view to create a status message'''
+    form_class = CreateStatusMessageForm
+    template_name = 'mini_fb/create_status_form.html'
+    
+    def get_context_data(self, **kwargs: Any) -> dict:
+        '''Add the profile to the context data'''
+        context = super().get_context_data(**kwargs)
+        context['profile'] = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        return context
+    
+    def form_valid(self, form):
+        '''Attach the profile to the status message'''
+        profile = get_object_or_404(Profile, pk=self.kwargs['pk'])
+        form.instance.profile = profile
+        return super().form_valid(form)
+    
+    def get_success_url(self) -> str:
+        '''Return the URL to redirect to on success.'''
+        return reverse('show_profile', kwargs={'pk': self.kwargs['pk']})
